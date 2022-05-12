0228f2ec11947819c9d7c94d04adbb8750326d11
renanlimaleite <renan.lima.softwares@gmail.com>
2022-05-12 17:48:00 -0300

    add body parser with limit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import { routes } from './routes'
+import bodyParser from 'body-parser'
 
 const PORT = 3333
 const app = express()
 
 app.use(cors())
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json())
 app.use(routes)
 
