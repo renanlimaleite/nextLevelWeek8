63a22dfc319396ffae20e1dd3fbf26bb080c574b
renanlimaleite <renan.lima.softwares@gmail.com>
2022-05-12 10:05:14 -0300

    add port variable

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,6 +9,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}`)
 })
